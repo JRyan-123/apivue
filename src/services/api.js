e6564201e385622416c7c693bebffe68d8b2b7cf
JRyan-123 <acostajoseryan4@gmail.com>
2025-12-05 21:07:16 +0800

    add headers

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,12 @@
-import { useAuthStore } from "@/stores/auth";
 import axios from "axios";
 
 
 const api = axios.create({
     baseURL: "http://localhost:8000/api",
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+    },
 });
 
 api.interceptors.request.use((config) => {
