076ceab88de7c7adc65ac46864ff79248354ab59
JRyan-123 <acostajoseryan4@gmail.com>
2026-01-11 12:49:01 +0800

    update login view page

--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -1,43 +1,46 @@
 <script setup>
+import { onMounted, reactive } from 'vue';
 import FormInput from '@/components/FormInput.vue';
-import router from '@/router';
 import { useAuthStore } from '@/stores/auth';
 import { storeToRefs } from 'pinia';
-import { onMounted, reactive } from 'vue';
+import router from '@/router';
 
+const auth = useAuthStore();
+const { errors, loading } = storeToRefs(auth)
 const formData = reactive({
     email: '',
     password: '',
-})
-async function login() {
-    const success = await auth.authenticate("login", formData);
-    if (success) {
-        router.push({ name: "dashboard" });
-    }
-}
+});
 
-const auth = useAuthStore();
-const { errors } = storeToRefs(auth);
 
-onMounted(() => errors.value = {});
+//button submit to store validation
+const login = async () => {
+    const success = await auth.authenticate('login', formData);
+    if (success) router.push({ name: 'dashboard' })
+};
+
+// clear old errors when page loads
+onMounted(() => errors.value = {})
 </script>
+
 <template>
-    <h1 class="text-3xl text-center mb-3">Login Form</h1>
+    <h1 class="text-3xl text-center mb-6">Login Form</h1>
     <main class="flex justify-center">
+        <form @submit.prevent="login" class="w-1/2">
+            <FormInput label="Email" type="email" placeholder="Enter Email" v-model="formData.email"
+                :error="errors.email?.[0]" />
 
-        <form @submit.prevent="login" class="w-1/2 ">
+            <FormInput label="Password" type="password" placeholder="Enter Password" v-model="formData.password"
+                :error="errors.password?.[0]" />
 
-            <FormInput label="Email" type="email" placeholder="Enter Email" :error="errors.email?.[0]"
-                v-model="formData.email" />
-            <FormInput label="Password" type="password" placeholder="Enter Password" :error="errors.password?.[0]"
-                v-model="formData.password" />
-
-            <button class="w-full px-6 py-2 text-yellow-500 border border-yellow-500 rounded-lg 
-                hover:bg-yellow-500 hover:text-black hover:scale-102 transition duration-200" :disabled="auth.loading">
-                {{ auth.loading ? 'Loading...' : 'Login' }}
+            <button type="submit" :class="[
+                'w-full px-6 py-2 rounded-lg border transition duration-200',
+                loading
+                    ? 'text-yellow-500 border-yellow-500 opacity-60 cursor-not-allowed'
+                    : 'text-yellow-500 border-yellow-500 hover:bg-yellow-500 hover:text-black hover:scale-102'
+            ]" :disabled="loading">
+                {{ loading ? 'Loading...' : 'Login' }}
             </button>
-
-
         </form>
     </main>
 </template>
---
+++ src/views/Auth/LoginViewOld.vue
@@ -0,0 +1,43 @@
+<script setup>
+import FormInput from '@/components/FormInput.vue';
+import router from '@/router';
+import { useAuthStore } from '@/stores/auth';
+import { storeToRefs } from 'pinia';
+import { onMounted, reactive } from 'vue';
+
+const formData = reactive({
+    email: '',
+    password: '',
+})
+async function login() {
+    const success = await auth.authenticate("login", formData);
+    if (success) {
+        router.push({ name: "dashboard" });
+    }
+}
+
+const auth = useAuthStore();
+const { errors } = storeToRefs(auth);
+
+onMounted(() => errors.value = {});
+</script>
+<template>
+    <h1 class="text-3xl text-center mb-3">Login Form</h1>
+    <main class="flex justify-center">
+
+        <form @submit.prevent="login" class="w-1/2 ">
+
+            <FormInput label="Email" type="email" placeholder="Enter Email" :error="errors.email?.[0]"
+                v-model="formData.email" />
+            <FormInput label="Password" type="password" placeholder="Enter Password" :error="errors.password?.[0]"
+                v-model="formData.password" />
+
+            <button class="w-full px-6 py-2 text-yellow-500 border border-yellow-500 rounded-lg 
+                hover:bg-yellow-500 hover:text-black hover:scale-102 transition duration-200" :disabled="auth.loading">
+                {{ auth.loading ? 'Loading...' : 'Login' }}
+            </button>
+
+
+        </form>
+    </main>
+</template>
